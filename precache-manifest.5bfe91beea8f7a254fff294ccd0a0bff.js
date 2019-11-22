255f10445c1b7905a94f39d82c78f087034aea2e
Atif Rabbani <atifrabbani1@yahoo.com>
2019-11-21 23:36:41 -0500

    Updates

---
+++ precache-manifest.5bfe91beea8f7a254fff294ccd0a0bff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f2c470a751a7804d9dde42617d8e2a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d5fb164a8ee88a4f01c6",
+    "url": "/clicky-game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c7718f706d1fa03d0878",
+    "url": "/clicky-game/static/js/2.0f377ec7.chunk.js"
+  },
+  {
+    "revision": "d5fb164a8ee88a4f01c6",
+    "url": "/clicky-game/static/js/main.7022be42.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  }
+]);
